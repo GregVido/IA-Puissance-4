de0db0f851f102b80f5837dab6fea7311999f04d
GregVido <gregvido17@gmail.com>
2026-08-13 16:56:32 +0200

    :sparkles: Ajout des variables redDepth et yellowDepth dans main()
    Les variables redDepth et yellowDepth ont été ajoutées à la fonction main() pour permettre la gestion de la profondeur de recherche pour chaque IA (Rouge et Jaune), en complément du type d'IA sélectionné.

--- Puissance 4/Puissance 4.cpp
+++ Puissance 4/Puissance 4.cpp
@@ -33,7 +33,7 @@ int main()
 
 	std::cout << "Puissance 4 - IA - By GregVido" << "\n\n";
 
-	int red, yellow;
+	int red, yellow, redDepth, yellowDepth;
 
 	std::cout << "Choisis le type d'IA pour le joueur Rouge (0: Facile, 1: Forte, 2: Imbattable) : ";
 	std::cin >> red;
