88e367150aee8a8b85fa5f1e0b68f759463b79c4
GregVido <gregvido17@gmail.com>
2026-08-16 22:33:40 +0200

    💄 Correction du message de profondeur de recherche
    Le message pour la sélection de la profondeur de recherche mentionne désormais "Rouge" au lieu de "Jaune". Un avertissement a été ajouté pour indiquer que l'augmentation de la profondeur allonge le temps de calcul.

--- Puissance 4/Puissance 4.cpp
+++ Puissance 4/Puissance 4.cpp
@@ -59,7 +59,7 @@ int main()
 
 		if (yellow == 1 || yellow == 2)
 		{
-			std::cout << "Choisis la profondeur de recherche pour le joueur Jaune (1-15) : ";
+			std::cout << "\nAttention, plus le chiffre est grand, plus le temps de calcul est long !\nChoisis la profondeur de recherche pour le joueur Rouge (1-15) : ";
 			std::cin >> yellowDepth;
 
 			yellowDepth = std::clamp(yellowDepth, 1, 15);
