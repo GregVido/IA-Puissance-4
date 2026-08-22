a0474b6dae71be14d53524e8ad34c8a91affd241
GregVido <gregvido17@gmail.com>
2026-08-22 09:26:33 +0200

    🔒️ Validation répétée de la saisie de colonne utilisateur
    La saisie de la colonne par l'utilisateur utilise désormais une boucle while pour répéter la demande tant que la colonne est invalide (hors bornes ou pleine). Des messages d'erreur spécifiques sont affichés selon le type d'erreur détectée.

--- Puissance 4/User.cpp
+++ Puissance 4/User.cpp
@@ -10,9 +10,17 @@ Move User::getMove(Board board, Box player)
 
 	board.draw();
 
-	std::cout << "Sur quel colonne voulez vous placer votre jeton ? (1 - " << board.getWidth() << ") : ";
-	std::cin >> move.column;
-	move.column--;
+	while (move.column < 0 || move.column >= board.getWidth() || board.isColumnFull(move.column))
+	{
+		std::cout << "Sur quel colonne voulez vous placer votre jeton ? (1 - " << board.getWidth() << ") : ";
+		std::cin >> move.column;
+		move.column--;
+
+		if (move.column < 0 || move.column >= board.getWidth())
+			std::cout << "Colonne invalide. ";
+		else if (board.isColumnFull(move.column))
+			std::cout << "Colonne pleine. ";
+	}
 
 	return move;
 }
